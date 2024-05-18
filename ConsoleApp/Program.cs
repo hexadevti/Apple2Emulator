2c416a226c1efa854360043d7994302fb144cd41
Luciano Faria <lucianobf@uol.com.br>
2024-05-18 15:39:52 -0300

    Winform app add

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,26 +1,27 @@
-﻿using Runtime;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Runtime;
 
 namespace ConsoleApp 
 {
     public  class Program 
     {
         public static bool run = true;
-        
 
+        [RequiresAssemblyFiles()]
         public static void Main(string[] args)
         {
-            //var appleRom = File.ReadAllBytes(@"C:\Users\luciano\repos\6502\ConsoleApp\apple1.rom");\
             var roms = new Dictionary<ushort, byte[]>();
-            
-            //roms.Add(0xf800, File.ReadAllBytes(@"/Users/lucianofaria/Desktop/Projects/6502/ConsoleApp/Apple II ROM Pages F8-FF - 341-0004 - Original Monitor.bin"));
-            roms.Add(0xf800, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0020 - Applesoft BASIC Autostart Monitor F800 - 2716.bin"));
-            roms.Add(0xf000, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0015 - Applesoft BASIC F000 - 2716.bin"));
-            roms.Add(0xe800, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0014 - Applesoft BASIC E800 - 2716.bin"));
-            roms.Add(0xe000, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0013 - Applesoft BASIC E000 - 2716.bin"));
-            roms.Add(0xd800, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0012 - Applesoft BASIC D800 - 2716.bin"));
-            roms.Add(0xd000, File.ReadAllBytes(System.IO.Directory.GetCurrentDirectory() + @"/ConsoleApp/Apple II+ - 341-0011 - Applesoft BASIC D000 - 2716.bin"));
+            string? assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (assemblyPath != null)
+                assemblyPath+= "/";            
 
-            //roms.Add(0xe000, File.ReadAllBytes(@"/Users/lucianofaria/Desktop/Projects/6502/ConsoleApp/basic.rom"));
+            roms.Add(0xf800, File.ReadAllBytes(assemblyPath + "roms/ApplesoftF800.rom"));
+            roms.Add(0xf000, File.ReadAllBytes(assemblyPath + "roms/ApplesoftF000.rom"));
+            roms.Add(0xe800, File.ReadAllBytes(assemblyPath + "roms/ApplesoftE800.rom"));
+            roms.Add(0xe000, File.ReadAllBytes(assemblyPath + "roms/ApplesoftE000.rom"));
+            roms.Add(0xd800, File.ReadAllBytes(assemblyPath + "roms/ApplesoftD800.rom"));
+            roms.Add(0xd000, File.ReadAllBytes(assemblyPath + "roms/ApplesoftD000.rom"));
             
             run = true;
             CPU cpu = new CPU(new State(), 0xffff, roms, true);
